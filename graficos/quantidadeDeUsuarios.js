bc4d47cca11bef805b3b313a193f46bb8042deba
ISAotelakoski <106194468+ISAotelakoski@users.noreply.github.com>
2024-10-31 09:24:39 -0300

    Add files via upload

--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -1,4 +1,4 @@
-import { getCSS } from "./comum.js";
+import { getCSS, tickfont} from "./comum.js";
 async function quantidadeDeUsuarios(){
     const url = `https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json`
     const res = await fetch(url)
@@ -27,13 +27,20 @@ async function quantidadeDeUsuarios(){
                 family: getCSS('--font'),
                 size: 30
             }
+        },
+        xaxis:{
+            tickfont: tickfont,
+            title: {
+                text: 'Nome das redes socias'
+            }
+
         }
     }
 
     const graficos = document.createElement('div');
     graficos.className  = 'graficos';
     document.getElementById('graficos-container').appendChild(graficos);
-    Plotly.newPlot(graficos,infos);
+    Plotly.newPlot(graficos,infos,layout);
 }
 
 quantidadeDeUsuarios();
